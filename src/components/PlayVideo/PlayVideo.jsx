e6cc1feeb3c2a09cadd9a2b94f1778ff53b8b0e4
qhvu-phan <qhvu.phan@gmail.com>
2024-05-22 16:46:41 +0700

    fix xoay man hinh

--- src/components/PlayVideo/PlayVideo.jsx
+++ src/components/PlayVideo/PlayVideo.jsx
@@ -6,6 +6,12 @@ import "./PlayVideo.css";
 const PlayVideo = (props) => {
   const videoNode = useRef(null);
   const [player, setPlayer] = useState(null);
+  window.addEventListener("orientationchange", function () {
+    // Lấy trình phát video
+    var player = videojs("my-video");
+    // Cập nhật kích thước của trình phát video
+    player.dimensions(window.innerWidth, window.innerHeight);
+  });
   useEffect(() => {
     if (videoNode.current) {
       const _player = videojs(videoNode.current, props);
@@ -20,7 +26,7 @@ const PlayVideo = (props) => {
 
   return (
     <div data-vjs-player>
-      <video ref={videoNode} className="video-js"></video>
+      <video ref={videoNode} id="my-video" className="video-js"></video>
     </div>
   );
 };
